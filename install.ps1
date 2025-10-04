2864532a28438fd8d5e9afbc1b18e125f01d451b
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2025-10-04 15:02:28 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ if (-not (Test-Path $ittPath)) {
     New-Item -ItemType Directory -Path $ittPath -Force | Out-Null
 }
 
-Invoke-WebRequest "https://github.com/itt-co/bin/releases/latest/download/installer.msi" -OutFile $installerPath
+Invoke-WebRequest "https://github.com/emadadeldev/bin/releases/latest/download/installer.msi" -OutFile $installerPath
 
 Start-Process msiexec.exe -ArgumentList "/i `"$installerPath`" /q" -NoNewWindow -Wait
 
